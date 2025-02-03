424fb996c194d8fa7ef0cd9f65142ff743f553f6
danielfelix45 <felixdaniel-developer@outlook.com>
2025-02-03 17:25:17 -0300

    Criação do arquivo Operadores e atualização do arquivo TiposVariaveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) {
+        System.lineSeparator();
+        // Operador de INCREMENTO
+        // int numero = 5;
+        // numero = numero + 1;
+        // numero++;
+        // numero += 1;
+        // System.out.println(numero);
+
+        // OPERADOR DE NEGAÇÃO
+        // boolean variavel = true;
+        // variavel = !variavel;
+        // System.out.println(variavel);
+
+        // OPERADOR TERNÁRIO
+        int a, b;
+        a = 10;
+        b = 10;
+        boolean resultado = a == b ? true : false;
+        System.out.println(resultado);
+    }
+}
